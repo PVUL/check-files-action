11a1487394d3b085b831dd3ca22d695784cd7992
Paul Yun <paul.a.yun@gmail.com>
2023-03-19 13:04:29 -0700

    Add gray-matter, add frontmatter check

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs');
-const { connected } = require('process');
+import core from '@actions/core';
+import github from '@actions/github';
+import fs from 'fs';
+import matter from 'gray-matter';
+import { connected } from 'process';
 
 async function checkFileExistence(path) {
   return fs.promises
@@ -16,12 +17,48 @@ async function checkFileExistence(path) {
     });
 }
 
+/**
+ * Returns frontmatter data from a filepath.
+ *
+ * @param path
+ * @returns frontmatter
+ */
+const getFrontmatterFromPath = (path: string) => {
+  const fileContents = fs.readFileSync(path, 'utf-8');
+  return matter(fileContents);
+};
+
+// check if we should proceed with a auto merging of PR
+// check if conditions are met for auto merging
+const checkFrontmatterFields = async (filePath) => {
+  const {
+    data: { postedAt, status },
+  } = getFrontmatterFromPath(filePath);
+
+  // condition 1.
+  // if status is 'scheduled post' - return true
+  const isScheduledPost = status === 'scheduled post';
+
+  // condition 2.
+  // if postedAt has a date in the past from current date - return true
+  const isPastPostedAt = new Date(postedAt) < new Date();
+
+  if (isScheduledPost && isPastPostedAt) {
+    core.info(`${filePath} is a scheduled for posting today`);
+    return true;
+  }
+
+  core.setFailed('No scheduled postings for today');
+  return false;
+};
+
 // create a function that checks if the file starts with a markdown header
 async function checkFileStartsWithHeader(filePath) {
   return fs.promises.readFile(filePath, 'utf8').then((fileContent) => {
     // remove all empty lines ad the beginning of the file
     fileContent = fileContent.replace(/^\s*\n/gm, '');
 
+    // we want to look at the front matter
     if (fileContent.startsWith('#')) {
       core.info(`File ${filePath} starts with a header`);
       return true;
@@ -34,18 +71,8 @@ async function checkFileStartsWithHeader(filePath) {
 
 (async () => {
   try {
-    // checkFileExistence('README.md');
-    // checkFileExistence('LICENSE');
-
-    console.log('process.env.filePath', process.env.filePath);
-    console.log('github env', github.env);
-
-    if (!checkFileStartsWithHeader('README.md')) {
-      // const token = core.getInput('repo-token')
-      // console.log(token)
-      // console.log(github)
-      // const octokit = new github.getOctokit(token)
-    }
+    const filePath = process.env.filePath; // ie. `_content/posts/_/2023-03-13_test.mdx`
+    checkFrontmatterFields(filePath);
   } catch (error) {
     core.setFailed(error.message);
   }
